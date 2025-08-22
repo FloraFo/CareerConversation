c43454203c1bee8ac65f0974f678f1a19972def7
filomena <filomenaforina@gmail.com>
2025-08-22 15:07:35 +0200

    v0.3.0

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 from pypdf import PdfReader
 import gradio as gr
 from tools import Tools
+from evaluation import Evaluator
 
 load_dotenv(override=True)
 
@@ -24,6 +25,7 @@ class Me:
                 self.linkedin += text
         with open("me/summary.txt", "r", encoding="utf-8") as f:
             self.summary = f.read()
+        self.evaluator = Evaluator(self.name, self.summary, self.linkedin)
 
     def handle_tool_call(self, tool_calls):
         results = []
@@ -37,35 +39,52 @@ class Me:
         return results
     
     def system_prompt(self):
-        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
-particularly questions related to {self.name}'s career, background, skills and experience. \
-Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
-You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
-Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
-If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
-If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+        prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, "
+        prompt += f"particularly questions related to {self.name}'s career, background, skills and experience. "
+        prompt += f"Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. "
+        prompt += f"You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. "
+        prompt += f"Be professional and engaging, as if talking to a potential client or future employer who came across the website. "
+        prompt += f"If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. "
+        prompt += f"If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+        prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        return prompt
 
-        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
-        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
-        return system_prompt
-    
+    def rerun(self, reply, message, history, feedback):
+        system_prompt = self.system_prompt()
+        updated_system_prompt = system_prompt + "\n\n## Previous answer rejected\nYou just tried to reply, but the quality control rejected your reply\n"
+        updated_system_prompt += f"## Your attempted answer:\n{reply}\n\n"
+        updated_system_prompt += f"## Reason for rejection:\n{feedback}\n\n"
+        messages = [{"role": "system", "content": updated_system_prompt}] + history + [{"role": "user", "content": message}]
+        response = self.gemini.chat.completions.create(model="gemini-2.5-flash-preview-05-20", messages=messages)
+        return response.choices[0].message.content
+
     def chat(self, message, history):
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.gemini.chat.completions.create(model="gemini-2.5-flash-preview-05-20", messages=messages, tools=self.tools.tools)
-            if response.choices[0].finish_reason=="tool_calls":
-                message = response.choices[0].message
-                tool_calls = message.tool_calls
+            response = self.gemini.chat.completions.create(model="gemini-2.5-flash-preview-05-20", messages=messages, tools=self.tools.tools_func)
+            if response.choices[0].finish_reason == "tool_calls":
+                msg = response.choices[0].message
+                tool_calls = msg.tool_calls
                 results = self.handle_tool_call(tool_calls)
-                messages.append(message)
+                messages.append(msg)
                 messages.extend(results)
+                reply = response.choices[0].message.content
             else:
+                reply = response.choices[0].message.content
+                evaluation = self.evaluator.evaluate(reply, message, history)
+                if evaluation.is_acceptable:
+                    print('Passed evaluation')
+                else:
+                    print('Failed evaluation')
+                    print(evaluation.feedback)
+                    reply = self.rerun(reply, message, history, evaluation.feedback)
                 done = True
-        return response.choices[0].message.content
+        return reply
     
 
 if __name__ == "__main__":
     me = Me()
-    gr.ChatInterface(me.chat, type="messages").launch()
+    gr.ChatInterface(fn=me.chat, title="Career Conversation with Filomena Forina", type="messages").launch()
     
--- tools.py
+++ tools.py
@@ -1,52 +1,11 @@
 import requests
 import os
-
+import json
 
 class Tools():
     def __init__(self):
-        self.record_user_details_json = {
-            "name": "record_user_details",
-            "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "email": {
-                        "type": "string",
-                        "description": "The email address of this user"
-                    },
-                    "name": {
-                        "type": "string",
-                        "description": "The user's name, if they provided it"
-                    }
-                    ,
-                    "notes": {
-                        "type": "string",
-                        "description": "Any additional information about the conversation that's worth recording to give context"
-                    }
-                },
-                "required": ["email"],
-                "additionalProperties": False
-            }
-        }
-
-        self.record_unknown_question_json = {
-            "name": "record_unknown_question",
-            "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "question": {
-                        "type": "string",
-                        "description": "The question that couldn't be answered"
-                    },
-                },
-                "required": ["question"],
-                "additionalProperties": False
-            }
-        }
-
-        self.tools = [{"type": "function", "function": self.record_user_details_json},
-                {"type": "function", "function": self.record_unknown_question_json}]
+        with open("tools_function.json") as f:
+            self.tools_func = json.load(f)
 
     def push(self, text):
         requests.post(
@@ -57,7 +16,6 @@ class Tools():
                 "message": text,
             }
         )
-
 
     def record_user_details(self, email, name="Name not provided", notes="not provided"):
         self.push(f"Recording {name} with email {email} and notes {notes}")
